01170a138d1af16cfe1bd6c9dc0ed649f3198d12
brayandres1 <brayanvelezlopez@hotmail.com>
2023-05-18 18:22:37 -0500

    Content page demo

--- MAUITest/App.xaml.cs
+++ MAUITest/App.xaml.cs
@@ -6,6 +6,11 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
-	}
+        var navPage = new NavigationPage(new ContentPageDemo());
+        navPage.BarBackgroundColor = Colors.Chocolate;
+        navPage.BarTextColor = Colors.White;
+        navPage.Title = "Demo MAUI";
+        MainPage = navPage;
+
+    }
 }
