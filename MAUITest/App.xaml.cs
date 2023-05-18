8558d691209a4dddec58245840b70cad39688224
brayandres1 <brayanvelezlopez@hotmail.com>
2023-05-18 18:48:02 -0500

    Flylayout demo

--- MAUITest/App.xaml.cs
+++ MAUITest/App.xaml.cs
@@ -6,11 +6,15 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-        var navPage = new NavigationPage(new ContentPageDemo());
-        navPage.BarBackgroundColor = Colors.Chocolate;
-        navPage.BarTextColor = Colors.White;
-        navPage.Title = "Demo MAUI";
-        MainPage = navPage;
+        //var navPage = new NavigationPage(new ContentPageDemo());
+        //navPage.BarBackgroundColor = Colors.Chocolate;
+        //navPage.BarTextColor = Colors.White;
+        //navPage.Title = "Demo MAUI";
+        //MainPage = navPage;
+
+        MainPage = new NavigationPage(new FlyoutPageDemo());
+
+
 
     }
 }
