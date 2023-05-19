2dfc0f3ef7d8db09d4d2d1e4bb599fe3dddc10d4
brayandres1 <brayanvelezlopez@hotmail.com>
2023-05-18 19:34:02 -0500

    Presentation  Controls Demo

--- MAUITest/App.xaml.cs
+++ MAUITest/App.xaml.cs
@@ -12,7 +12,7 @@ public partial class App : Application
         //navPage.Title = "Demo MAUI";
         //MainPage = navPage;
 
-        MainPage = new NavigationPage(new TabbedPageDemo());
+        MainPage = new NavigationPage(new PresentationControlsDemo());
 
 
 
